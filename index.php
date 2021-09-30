14b81ac1e26c0051d15f79e7bf35a384fa7d5e7c
nrlhuda67 <81946346+nrlhuda67@users.noreply.github.com>
2021-09-30 10:58:07 +0800

    baru

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan kedua</title>
+</head>
+<body>
+    <h1> Hello Wolrd </h1>
+    <?php
+    $nama = 'Huda';
+    $umur = 20 + 1;
+
+    echo "$nama berumur $umur tahun";
+    ?>
+</body>
+</html>
